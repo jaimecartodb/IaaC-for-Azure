027307f7cd6244aee6a09c7ae5f948aff9cf757b
jaimecartodb <jaimedemoranavarro@gmail.com>
2024-06-22 19:43:21 +0200

    Incluir Azure Monitor y Log Analytics 5

--- main.tf
+++ main.tf
@@ -120,9 +120,12 @@ resource "azurerm_linux_virtual_machine_scale_set" "example" {
   name                = "example-vmss"
   location            = azurerm_resource_group.myrg.location
   resource_group_name = azurerm_resource_group.myrg.name
+
   sku                 = "Standard_DS1_v2"
-  admin_username      = "adminuser"
-  admin_password      = "Password1234!"
+  instances           = 2
+
+  admin_username = "adminuser"
+  admin_password = "Password1234!"
 
   source_image_reference {
     publisher = "Canonical"
@@ -144,6 +147,13 @@ resource "azurerm_linux_virtual_machine_scale_set" "example" {
       subnet_id = azurerm_subnet.mysubnet.id
       primary   = true
     }
+  }
+
+  admin_ssh_key {
+    username   = "adminuser"
+    public_key = <<EOF
+ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDT5Y3cpT6P7v/wszaMIPK61ZvsmS7fccix/lEDXOrccq1CKk4tXAL4uRY9OdvWoFGeH04D4RFrGVX89fYGhFa+KJD0SOpWztWsK50xsLhzId6mFabS24JE/ZkVrGmQo4K+kqbJul3WwRhk4N+t8uTXGLS3S3hKWUn3h0DqIDulKDt/XBuEYl4opsbllLi/VQKVU5gbcOJPYZjLKcMS5uAQipnt66cHMuWK73+teVLQyKa48uYaXE89tT4w2H9R7Xyx5aAl6J2WlJmmHf97sDY5rC7I6ENFIUAsCo1zhsI39MQaSGQb2V4UnFmpQVDFH+/AC+NZbZeWfHirCgDtAUptc9UwYyHSMzROmrddzd3CBpZZl/YTgNxfJa/StsfjXTRZLz87uMY8X/SXU6uSw25nL1UrCy+q9mP1PEYwVUZS65vg1IUI38lWVmgfKD+47LYDVm4Yumb5993B0xhmWMVh30hM8nZcAqCV8Qtwjr12KfOqFuViZ+V32jE1+eiXYuETDK7yh5MPKQrmLNT8mCF0iT8oDYMF3Jp4CFrsaCQGBbOuAFeyieuHSNs9vKdqXkQLGXo2Rvrx1EwpxARVwgauuGFxt7cYU8y3vKmDmM64yd1FdfEPrfsWtgPm1hxQFucuocvptaf8m0ogB0rZwemPz3JyBCZoLOYacdWkgTHV6Q== jaimedemoranavarro@gmail.com
+EOF
   }
 }
 
