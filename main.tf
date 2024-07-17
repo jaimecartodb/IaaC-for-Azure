c36fab02343e60ebef41a711a84b40d483ea62f1
jaimecartodb <jaimedemoranavarro@gmail.com>
2024-07-17 11:58:12 +0200

    Con cambios en el main.tf

--- main.tf
+++ main.tf
@@ -118,7 +118,7 @@ resource "azurerm_key_vault_secret" "example" {
 }
 
 resource "azurerm_linux_virtual_machine_scale_set" "example" {
-  name                = "example-vmss2"
+  name                = "example-vmss"
   location            = azurerm_resource_group.myrg.location
   resource_group_name = azurerm_resource_group.myrg.name
   sku                 = "Standard_DS1_v2"
